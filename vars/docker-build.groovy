303b76d919224dafbedd717220c783baa1daa93c
Pragya Agrawal <pragya222@gmail.com>
2025-01-28 14:34:37 -0600

    Update docker-build.groovy

--- vars/docker-build.groovy
+++ vars/docker-build.groovy
@@ -1,5 +1,5 @@
 def call(String ProjectName,String ImageTag,String DockerHubUser){
    echo "this is building the code"
    sh "whoami"
-   sh "docker build -t "${DockerHubUser}"/"${ProjectName}":"${ImageTag}" ."
+   sh "docker build -t "${ProjectName}":"${ImageTag}" ."
 }
